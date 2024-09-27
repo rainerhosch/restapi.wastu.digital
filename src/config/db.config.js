6016a1e06ca700a2a4b34f9f0765e1f83787fd1d
rz.oktan <rz.oktan@gmail.com>
2024-09-27 07:38:13 +0700

    update

--- src/config/db.config.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DATABASE: "wastudig_simak",
-    PORT: '3306'
-};
-module.exports = dbConfig;
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config");
+require('dotenv').config()
 
+ // Start of Selection
 const conn = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DATABASE,
-    port: dbConfig.PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 conn.connect(function (err) {
